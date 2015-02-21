9e5721cb0dde132ddb370f786a37f0afab58654c
Gabi Davar <grizzly.nyo@gmail.com>
2015-02-21 16:46:11 +0200

    use setuptools for scripts and deps

--- setup.py
+++ setup.py
@@ -2,17 +2,33 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
 
-from distutils.core import setup
+from setuptools import setup
 
 long_description = open('README.rst').read()
 
 exec(open('bloscpack/version.py').read())
 
+install_requires = [
+    'blosc>=1.2.0',
+    'numpy'
+]
+
+tests_require = [
+    'nose',
+    'cram>=0.6',
+    'mock',
+    'coverage',
+]
+
 setup(
     name = "bloscpack",
     version = __version__,
     packages = ['bloscpack'],
-    scripts = ['blpk'],
+    entry_points = {
+        'console_scripts' : [
+            'blpk = bloscpack.cli:main',
+        ]
+    },
     author = "Valentin Haenel",
     author_email = "valentin@haenel.co",
     description = "Command line interface to and serialization format for Blosc",
@@ -20,12 +36,17 @@ setup(
     license = "MIT",
     keywords = ('compression', 'applied information theory'),
     url = "https://github.com/blosc/bloscpack",
+    install_requires = install_requires,
+    extras_require = dict(tests=tests_require),
+    tests_require = tests_require,
     classifiers = ['Development Status :: 3 - Alpha',
                    'Environment :: Console',
                    'License :: OSI Approved :: MIT License',
+                   'Operating System :: Microsoft :: Windows',
                    'Operating System :: POSIX',
                    'Programming Language :: Python',
                    'Topic :: Scientific/Engineering',
+                   'Topic :: System :: Archiving :: Compression',
                    'Topic :: Utilities',
                   ],
      )
